5cad53ff10ae38b0d597c31f2ea953841ad27799
Nguyen Cong Khoa <khoanguyen060523@gmail.com>
2026-03-24 13:31:54 +0700

    var,comment,built-in

---
+++ README.md
@@ -0,0 +1,3 @@
+# JavaScript Basics
+
+Đây là repository lưu trữ các đoạn code, tài liệu và ghi chú trong quá trình học JavaScript từ cơ bản.
---
+++ main.js
@@ -0,0 +1,12 @@
+var fullName = "John Doe";
+console.log(fullName);
+console.warn(fullName);
+console.error(fullName);
+confirm('hello');
+prompt('what is your name?');
+setTimeout(function() {
+    console.log('This message will be displayed after 1 second');
+}, 1000)
+setInterval(function() {
+    console.log('This message will be displayed every 1 second');
+}, 1000)

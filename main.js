c1bde0ae45517497097555fdc859fcfcd2cabb57
Nguyen Cong Khoa <khoanguyen060523@gmail.com>
2026-03-29 12:08:37 +0700

    object

--- main.js
+++ main.js
@@ -1,2 +1,21 @@
-var languages = ['javacript','javacript','javacript']
-console.log(languages)
+function User(firstName, lastName, avater) {
+    this.firstName = firstName
+    this.lastName = lastName
+    this.avater = avater
+
+    this.getName = function() {
+        return `${this.firstName} ${this.lastName}`
+    }
+}
+
+User.prototype.className = 'F8'
+
+var author = new User('Huy', 'Nguyen', 'avater.jpg')
+var user = new User('Huy', 'Nguyen', 'avater.jpg')
+
+author.title = 'Developer'
+user.comment = 'Hello'
+
+console.log(author)
+console.log(user)
+

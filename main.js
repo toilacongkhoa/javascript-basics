9c0641008c99959157fba2261685f63bf047041a
Nguyen Cong Khoa <khoanguyen060523@gmail.com>
2026-03-30 10:08:04 +0700

    if else

--- main.js
+++ main.js
@@ -1,21 +1,30 @@
-function User(firstName, lastName, avater) {
-    this.firstName = firstName
-    this.lastName = lastName
-    this.avater = avater
+var date = 2
 
-    this.getName = function() {
-        return `${this.firstName} ${this.lastName}`
-    }
+switch(date){
+    case 2:
+        console.log('Hôm nay là thứ 2')
+        break
+    case 3:
+        console.log('Hôm nay là thứ 3')
+        break
+    case 4:
+        console.log('Hôm nay là thứ 4')
+        break
+    case 5:
+        console.log('Hôm nay là thứ 5')
+        break
+    case 6:
+        console.log('Hôm nay là thứ 6')
+        break
+    case 7:
+        console.log('Hôm nay là thứ 7')
+        break
+    case 8:
+        console.log('Hôm nay là chủ nhật')
+        break
+    default:
+        console.log('Không phải ngày trong tuần')
 }
 
-User.prototype.className = 'F8'
-
-var author = new User('Huy', 'Nguyen', 'avater.jpg')
-var user = new User('Huy', 'Nguyen', 'avater.jpg')
-
-author.title = 'Developer'
-user.comment = 'Hello'
-
-console.log(author)
-console.log(user)
-
+var a = 10 > 2 ? 10 : 2
+console.log(a)
